084ba568ed9ef66d999104d309228983acc7573b
arya-bhanu <aryacreative.dev@gmail.com>
2024-07-15 08:20:35 +0700

    feat: migration prisma

---
+++ prisma/migrations/20240715011714_init/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updatedAt` on the `Post` table. All the data in the column will be lost.
+  - You are about to alter the column `email` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `name` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "updatedAt",
+ALTER COLUMN "createdAt" SET DATA TYPE TIMESTAMP(6);
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "email" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "name" SET DATA TYPE VARCHAR(255);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
